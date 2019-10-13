7507d7858ce35045574ce64f037acec5e921e2a0
Pawanrt31 <44312151+Pawanrt31@users.noreply.github.com>
2019-10-13 10:59:01 +0530

    Update del.php

--- del.php
+++ del.php
@@ -1,35 +1,27 @@
+//Code to delete a medicine
 <?php 
 session_start();
-
 $medcode=0;
 $errors=array();
-
 $db=mysqli_connect('localhost','root','','registration');
-
+//checking if the delete button is pressed or not
 if(isset($_POST['delete']))
 {
 	$medcode=mysqli_real_escape_string($db, $_POST['medcode']);
-
-if (empty($medcode)) 
-{ 
-array_push($errors, "Medicine code is required"); 
-}
-$user_check_query = "SELECT * FROM medicine WHERE med_code='$medcode' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $medicine = mysqli_fetch_assoc($result);
-  
-  //if (!$medicine) { // if user exists
-  //  if ($medicine['med_code'] !== $medcode) {
-  //    array_push($errors, "Medicine doesn't exists");
-   // }
-  //}
- if(count($errors)==0)
- {
+	if (empty($medcode)) 
+	{ 
+	array_push($errors, "Medicine code is required"); 
+	}
+	$user_check_query = "SELECT * FROM medicine WHERE med_code='$medcode' LIMIT 1";
+  	$result = mysqli_query($db, $user_check_query);
+  	$medicine = mysqli_fetch_assoc($result);
+  	if(count($errors)==0)
+ 	{
 	 $query = "DELETE FROM medicine where med_code=$medcode";
-  	mysqli_query($db, $query);
-  	$_SESSION['success'] = "Successfully removed a medicine";
-  	header('location: home.php');
-  }
+  	 mysqli_query($db, $query);
+  	 $_SESSION['success'] = "Successfully removed a medicine";
+  	 header('location: home.php');
+  	}
 }
   ?>
 <!DOCTYPE html>
@@ -47,9 +39,8 @@ if(id1[0].value.length==0)
  id1.focus();
  return false;
 }
-
 else
-	return true;
+return true;
 }
 </script>
 </head>
@@ -68,7 +59,5 @@ else
   	</div>
 	<a href="home.php">Go back</a>
 </form>   
-
-		
 </body>
 </html>
